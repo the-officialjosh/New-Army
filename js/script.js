daf5d91a7730f700cb760124c232503dc359321a
JOSH <onyemajoshua892@gmail.com>
2023-06-18 21:30:13 +0100

    Fixed captitalization

--- js/script.js
+++ js/script.js
@@ -5,3 +5,45 @@ btnEl.addEventListener("click", (e) => {
   document.querySelector(".menu-icon-wrapper").classList.toggle("open");
   headerEl.classList.toggle("open");
 });
+
+const convertTitleCase = (title) => {
+  const nonCapitalizedWords = [
+    "a",
+    "an",
+    "and",
+    "as",
+    "at",
+    "but",
+    "by",
+    "for",
+    "from",
+    "in",
+    "nor",
+    "of",
+    "on",
+    "or",
+    "the",
+    "to",
+    "with",
+  ];
+  const titleCase = title
+    .toLowerCase()
+    .split(" ")
+    .filter((word) => word)
+    .map((word, i) =>
+      nonCapitalizedWords.includes(word) && i !== 0
+        ? word
+        : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(" ");
+  return titleCase;
+};
+
+const newTitle = document.querySelector(".header__title--secondary");
+const newSub = document.querySelectorAll(".heading-secondary");
+
+newTitle.textContent = convertTitleCase(newTitle.textContent);
+
+newSub.forEach((el) => {
+  el.textContent = convertTitleCase(el.textContent);
+});
